6960d84632f7d34f58073c5426cee026bbcaa2fa
Paul K <pkfis@outlook.com>
2020-01-28 02:48:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jabba the Hutt"
+  end
+
+  get '/hometown' do
+    'My hometown is Tatooine'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is Lapti-Nek'
+  end
+
 end
